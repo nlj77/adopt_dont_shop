7580c4de67c31001e2f3fbd10236b263ab2974ae
Gauri Joshi <gauri728@gmail.com>
2022-07-19 18:39:11 -0700

    changes admin applications_Spec url paths to say adminS plural

--- spec/features/admin/application_spec.rb
+++ spec/features/admin/application_spec.rb
@@ -9,17 +9,36 @@ RSpec.describe "admin application show page" do
 # Then I'm taken back to the admin application show page
 # And next to the pet that I rejected, I do not see a button to approve or reject this pet
 # And instead I see an indicator next to the pet that they have been rejected
+    it 'can approve a pet for adoption' do
+        new_applicant = Applicant.create!(name: "Test", address: "5555 Test Avenue", city: "Denver", state: "CO", zip: 55555, names_pets_wanted: "Fido", description: "they love pets!", application_status: "In Progress")
+        shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        fido = shelter_1.pets.create(name: 'Fido', breed: 'Beagle', age: 5, adoptable: true)
+        ApplicantPet.create!(pet: fido, applicant: new_applicant)
+        visit "/admins/applications/#{new_applicant.id}"
+        within "#pet_#{pet_1.id}" do
+            click_button 'Approve'
+        end
+    
+        expect(current_path).to eq("/admins/applications/#{new_applicant.id}")
+    
+        within "#pet_#{pet_1.id}" do
+            expect(page).to_not have_button('Reject')
+            expect(page).to_not have_button('Approve')
+            expect(page).to have_content('Approved')
+        end
+    end
+
     it 'can reject a pet for adoption' do
         new_applicant = Applicant.create!(name: "Test", address: "5555 Test Avenue", city: "Denver", state: "CO", zip: 55555, names_pets_wanted: "Fido", description: "they love pets!", application_status: "In Progress")
         shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
         fido = shelter_1.pets.create(name: 'Fido', breed: 'Beagle', age: 5, adoptable: true)
         ApplicantPet.create!(pet: fido, applicant: new_applicant)
-        visit "/admin/applications/#{new_applicant.id}"
+        visit "/admins/applications/#{new_applicant.id}"
         within "#pet_#{pet_1.id}" do
             click_button 'Reject'
         end
     
-        expect(current_path).to eq("/admin/applications/#{new_applicant.id}")
+        expect(current_path).to eq("/admins/applications/#{new_applicant.id}")
     
         within "#pet_#{pet_1.id}" do
             expect(page).to_not have_button('Reject')
